2a5ccb8add6f429f01aa659415cabc33540a5a50
Dante Dekstop <danteantu5@gmail.com>
2018-10-12 20:49:17 -0300

    ignore not relevant files

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,34 +1,63 @@
 package sample;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.List;
 
 public class Controller {
     Stage tempStage = new Stage();
 
+
+    void createFileField(ObservableList list, String text) {
+        TextField temp = new TextField(text);
+        main_grid.add(temp, 1, 0);
+    }
+
     @FXML
-    TextField input_location;
+    GridPane main_grid;
+
+    @FXML
+    HBox input_files;
+
+    @FXML
+    ListView input_location;
 
     @FXML
     TextField output_location;
 
     @FXML
     void searchFile() {
+        input_location.setPlaceholder(new Text(""));
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Choose file(s)");
-        File file = fileChooser.showOpenDialog(tempStage);
+        List<File> files = fileChooser.showOpenMultipleDialog(tempStage);
+
         //Check if the user entered files
-        if(file != null) {
-            String filePath = file.getPath();
-            input_location.setText(filePath);
+        if(files != null) {
+            String[] filesPaths = new String[files.size()];
+            //Insert file's paths
+            for(int i = 0; i < files.size(); i++) {
+                filesPaths[i] = files.get(i).getPath();
+                ObservableList elements = input_location.getItems();
+                elements.add(filesPaths[i]);
+
+                //TODO: add dinamic list to show a list of the items
+            }
         }
         else {
-            input_location.setText("");
+            input_location.setPlaceholder(new Text(""));
         }
     }
 
@@ -50,7 +79,7 @@ public class Controller {
 
     @FXML
     void compress() {
-        System.out.println("Extracting '"+input_location.getText()+"' to '"+output_location.getText()+"'...");
+        System.out.println("Extracting '"+"' to '"+"'...");
     }
 
 }
